f765635452bd9eb5dc9d6547c2d6d745bec06cee
Niki-home <nikmastera@gmail.com>
2018-03-29 00:59:14 +0300

    home

--- CSharp-OOP-Advanced/Generics/05.GenericCountMethodStrings/05.StartUp.cs
+++ CSharp-OOP-Advanced/Generics/05.GenericCountMethodStrings/05.StartUp.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _05.GenericCountMethodStrings
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var list = new List<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                //var box = new Box<int>(int.Parse(Console.ReadLine()));
+                list.Add(Console.ReadLine());
+            }
+
+            string input = Console.ReadLine();
+            int result = Compare(list, input);
+            Console.WriteLine(result);
+        }
+
+        public static int Compare<T>(List<T> list, string compareStr)
+            where T : IComparable
+        {
+            int cntr = 0;
+
+            foreach (T item in list)
+            {
+                if (item.CompareTo(compareStr) > 0)
+                {
+                    cntr++;
+                }
+            }
+
+            return cntr;
         }
     }
 }
