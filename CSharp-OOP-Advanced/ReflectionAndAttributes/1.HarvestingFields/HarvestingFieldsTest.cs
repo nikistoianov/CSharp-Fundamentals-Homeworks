760f1e2ea9b6469291a6c3a3d75c870b079b48a8
Nikolay <nikmastera@gmail.com>
2018-04-04 17:45:03 +0300

    Reflection and Attributes

---
+++ CSharp-OOP-Advanced/ReflectionAndAttributes/1.HarvestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,33 @@
+﻿ namespace P01_HarvestingFields
+{
+    using System;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            //TODO put your reflection code here
+            var type = typeof(HarvestingFields);
+            string command;
+            while ((command = Console.ReadLine()) != "HARVEST")
+            {                
+                var fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+                foreach (var field in fields)
+                {
+                    if (command == "all" || 
+                        (command == "private" && field.IsPrivate) || 
+                        (command == "protected" && field.IsFamily) || 
+                        (command == "public" && field.IsPublic))
+                    {
+                        var modifier = field.Attributes.ToString().ToLower();
+                        if (modifier == "family")
+                            modifier = "protected";
+                        Console.WriteLine($"{modifier} {field.FieldType.Name} {field.Name}");
+                    }
+                    
+                }
+            }
+        }
+    }
+}
---
+++ CSharp-OOP-Advanced/ReflectionAndAttributes/2.BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,27 @@
+﻿namespace P02_BlackBoxInteger
+{
+    using System;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            //TODO put your reflection code here
+            //var bb = new BlackBoxInteger(1,);
+            var blackBox = Activator.CreateInstance(typeof(BlackBoxInteger), true);
+            var type = blackBox.GetType();
+            var field = type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance)[0];
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var args = command.Split('_');
+                var method = type.GetMethod(args[0], BindingFlags.NonPublic | BindingFlags.Instance);
+                object[] param = { int.Parse(args[1]) };
+                method.Invoke(blackBox, param);
+                var result = field.GetValue(blackBox);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
