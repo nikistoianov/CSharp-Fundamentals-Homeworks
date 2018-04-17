f78bf3aecbfe26909b08d171dee740048b714812
Niki-home <nikmastera@gmail.com>
2018-04-17 07:46:52 +0300

    Last Army

---
+++ CSharp-OOP-Advanced/ExamPrepar1/Last Army/Entities/Ammunitions/Ammunition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class Ammunition : IAmmunition
+{
+    public string Name { get; private set; }
+    public double Weight { get; private set; }
+    public double WearLevel { get; }
+    public void DecreaseWearLevel(double wearAmount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Ammunition(string name, double weight)
+    {
+        this.Weight = weight;
+        this.Name = name;
+    }
+}
